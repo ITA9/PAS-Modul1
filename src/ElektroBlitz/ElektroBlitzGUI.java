138c1aeea82bc5d0bd9ccaa8506c06da6898eabc
Max Vincent Goldgamer <max-vincent@goldgamer.de>
2019-10-27 22:37:41 +0100

    -

---
+++ src/ElektroBlitz/ElektroBlitzGUI.java
@@ -0,0 +1,21 @@
+package ElektroBlitz;
+import java.util.Scanner;
+
+/**
+ * @author Max Vincent Goldgamer %% Gabriel Akrutat
+ *
+ */
+public class ElektroBlitzGUI {
+
+
+	public ElektroBlitzGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
